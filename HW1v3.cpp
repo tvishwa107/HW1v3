6cc7e3a830170935d8e11d210dcb7fd704690e88
tvishwa107 <tvishwa107@gmail.com>
2015-10-10 20:11:01 -0500

    Modified pointers, test conditions, etc.

--- HW1v3.cpp
+++ HW1v3.cpp
@@ -40,9 +40,9 @@ public:
 #pragma pack(pop) 
 
 
-void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int k);
+void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int *k);
 
-void buffer_to_file(char *Buf, uint64 i, int k, FILE *outHandle);
+void buffer_to_file(char *Buf, uint64 i, int *k, FILE *outHandle);
 
 void sort_file()
 {
@@ -53,6 +53,20 @@ void sort_file()
 }
 
 
+//When is makeHandle called? When the buffer becomes full in the middle of processing
+FILE *make_Handle(int *k, FILE *optHandle = NULL)
+{
+	if (optHandle)
+	{
+		fclose(optHandle);
+		std::cout << "\nclosing file " << *k << " and opening file " << (*k)+1<<std::endl;
+	}
+	const std::string kStr = std::to_string(*k);
+	FILE *outHandle = fopen(kStr.c_str(), "wb");
+	std::cout<<"Opened file " << (*k) + 1 << std::endl;
+	return outHandle;
+}
+
 void readMyHandle(FILE *myHandle) //reads the file till buffer ends ONCE. This is the next place to be edited
 {
 	//std::cout << "\nEntered RMH\n";
@@ -60,18 +74,17 @@ void readMyHandle(FILE *myHandle) //reads the file till buffer ends ONCE. This i
 	char *outBuf = (char *)malloc(OUT_BUF*sizeof(char));
 	size_t success;
 	int i = 0;
+	int *k = &i;
 	try{
-		while (!feof(myHandle)) //the end of the while loop is hitting the end of the buffer (ie full MAX_RAM absorbed)
-		{
+		do {
 			//std::cout << "\nEntering retVal loop\n";
 			success = fread(buf, sizeof(char), MAX_BUF, myHandle);
 			//std::cout << "\n Position is " << ftell(myHandle) << " \n";
 			//At this point, call new function that does the parsing
-			parseBuf(buf, MAX_BUF, myHandle, outBuf, i);
-			i++;
-			std::cout << "\i is now " << i <<	 "\n";
+			parseBuf(buf, MAX_BUF, myHandle, outBuf, k);
+			//std::cout << "\ni is now " << *k <<	 "\n";
 			memset(buf, 0, MAX_BUF);
-		}
+		} while (success == MAX_BUF);
     }
 	catch(int e)
 	{
@@ -81,42 +94,65 @@ void readMyHandle(FILE *myHandle) //reads the file till buffer ends ONCE. This i
 }
 
 
-uint64 write_to_buffer(HeaderGraph *hg, char *outBuf, uint64 i, int k, FILE *outHandle)
+uint64 write_to_buffer(HeaderGraph *hg, char *outBuf, uint64 i, int *k, FILE *outHandle)
 {		
 	//i is the number of HeaderGraphs written, k is the number n of the file name eg. 0,1,2
-	//need to maintain a count on current position
-   
+	
+//option 1: do what he did, but mine's already slower
+//2: rerun mine, with a limit on how big the file can be. i'll try 2
+
+	if(ftell(outHandle)+sizeof(HeaderGraph)>=MAX_BUF)
+	{
+		//std::cout << "outHandle = ";
+		//std::cout << ftell(outHandle);
+		//std::cout << "\ni=" << i << std::endl;
+		//system("pause");
+		buffer_to_file(outBuf, i, k, outHandle); //copy buffer to file
+												 //std::cout << "Done";
+		++*k;
+		outHandle = make_Handle(k,outHandle);
+		memset(outBuf, 0, OUT_BUF);   //reset buffer
+		return 0; //reset count of number in buffer
+//once you've added this, after the first time, you need a way to signal that it's time to close and open a new file
+	}
 	if (((i+1)*sizeof(HeaderGraph)) > OUT_BUF)
 	{ 
 		//std::cout << "\ni=" << i << std::endl;
+		
 		buffer_to_file(outBuf, i, k, outHandle); //copy buffer to file
+		
 		//std::cout << "Done";
+		//system("pause");
+		
 		memset(outBuf, 0, OUT_BUF);   //reset buffer
 		return 0; //reset count of number in buffer
 	}
 	else //buffer can hold one more at least
 	{
+		//std::cout << "\nEse in W2B\n";
 	    memcpy(outBuf + i*sizeof(HeaderGraph), hg, sizeof(HeaderGraph)); 
+		//std::cout << "\ni = " << i;
 		return ++i;
 	}
 }
 
-void buffer_to_file(char *buf, uint64 i, int k, FILE *outHandle)
+
+void buffer_to_file(char *buf, uint64 i, int *k, FILE *outHandle)
 {
 	size_t succ = fwrite(buf, sizeof(HeaderGraph), i, outHandle);
-	if(succ!=i)
+	if(succ==0)
 	{
 		//std::cout << "succ = " << succ << " and i = " << i;
 		//system("pause");
-		exit(1);
+		return;
 	}
 	//else std::cout << "succ = i = " << succ << " " << i << std::endl;
 	return;
 
-//5:40PM failing because I'm not closing any file handles, annd I'm not maintaining the position in the file either
+
 }
 
-void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int k)
+void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int *k)
 {
 	
 	uint64 off = 0;
@@ -126,12 +162,8 @@ void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int k)
 	signed long int mynewpos = 0;
 	//std::cout << "\noff = "<<off<<" and size = "<<size<<std::endl;
 	
-	const std::string kStr = std::to_string(k);
-	//std::cout << "\nWriting to file " << k << std::endl;
-	FILE *outHandle = fopen(kStr.c_str(), "wb");
-	//if (off < size - sizeof(HeaderGraph))
-	//	std::cout << "\nCondition met with off, size-HG = " << off << " " << size-sizeof(HeaderGraph)<<" \n";
-	//std::cout << "\nPosn at beginning of loop is is " << ftell(myHandle) << std::endl;
+
+	FILE *outHandle = make_Handle(k);
 
 	while (off < size - sizeof(HeaderGraph))
 	{
@@ -166,11 +198,7 @@ void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int k)
 			
 			int res;
 			size_t succ;
-			//mypos = ftell(myHandle);  mypos is always MAX_RAM, so subtract that from off each time. Subtract That
-			
-			//std::cout << "\nmypos = " << mypos <<" and off = "<<off<<"\n";
-		//	std::cout << off - sizeof(HeaderGraph) - hg->len*sizeof(uint64);
-			//mynewpos -= SEEK_CUR+(sizeof(HeaderGraph) + hg->len*sizeof(uint64))-off-1;
+
 			mynewpos = off - (sizeof(HeaderGraph) + hg->len*sizeof(uint64));
 			//std::cout << "\nval = " <<  off- (sizeof(HeaderGraph) + hg->len*sizeof(uint64));
 			res = fseek(myHandle, -1*(MAX_BUF-mynewpos), SEEK_CUR);
@@ -182,7 +210,7 @@ void parseBuf(char *buf, int bytesToRead, FILE* myHandle, char *outBuf, int k)
 	}
 	//std::cout << "Failed headertest: off = " <<off<<"ooff = "<<ooff<<", size - HeaderGraph = "<<size<<"-"<<sizeof(HeaderGraph);
 	fseek(myHandle, -1 * (MAX_BUF - (ooff)), SEEK_CUR);
-	std::cout<<std::endl<<ftell(myHandle)<<"\n";
+	//std::cout<<std::endl<<ftell(myHandle)<<"\n";
 	fclose(outHandle);
 	return;
 }
